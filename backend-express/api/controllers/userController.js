64b9b08d7e7ab8d328ed12bdbcd4191fc0dbd281
ZarifS <zarif.shahriar@gmail.com>
2019-11-12 22:27:12 -0500

    added transaction pull into users model

--- backend-express/api/controllers/userController.js
+++ backend-express/api/controllers/userController.js
@@ -100,16 +100,64 @@ const calculateUserBalance = (accounts) => {
   return sum;
 };
 
+// Pulls historical data for a institution to gather user transactions
 export const setTransactionsForUser = async (req, res) => {
   const { startDate, endDate, institutionName } = req.body;
-  const { accessToken } = req.user.links.get(institutionName);
-  let allTransactions = [];
+  const user = req.user
+  const { accessToken } = user.links.get(institutionName);
   try {
-    const transactions = await getTransactions(accessToken, startDate, endDate);
-    allTransactions = allTransactions.concat(transactions);
+    let transactions = await getTransactions(accessToken, startDate, endDate);
+    transactions.map(transaction => {
+      let transactionObject = {
+        name: transaction.name,
+        category: transaction.category,
+        amount: transaction.amount,
+        accountID: transaction.account_id,
+        date: transaction.date,
+        pending: transaction.pending,
+        pending_id: transactions.pending_transaction_id,
+        _id: transaction.transaction_id,
+        currency: transaction.iso_currency_code
+      }
+      // Check for duplicated transactions, if already exists, replace it.
+      if (user.transactions.id(transaction.transaction_id)) {
+        console.log('Transaction already exists, replacing!')
+        user.transactions.pull(transaction.transaction_id)
+      }
+      user.transactions.push(transactionObject)
+    })
+    let result = await user.save()
+    return res.status(200).json({ message: 'User Transactions Added Successfully.', user: result });
   }
   catch (err) {
+    console.log(err)
     return res.status(400).json([{ message: err.message }]);
   }
-  res.status(200).json(allTransactions);
 };
+
+export const testAPI = async (req, res) => {
+  req.user.transactions = []
+  let result = await req.user.save()
+  // const id = mongoose.Types.ObjectId().toString();
+  // console.log('ID:', id)
+  // let object = {
+  //   "category": [
+  //     "Food and Drink",
+  //     "Restaurants"
+  //   ],
+  //   "name": "TESTING TESTING TESTING 5",
+  //   "amount": 89.4,
+  //   "accountID": "zpwozvo6X5cVbg9z8EEJHQKlqrevnNuoGWpe8",
+  //   "date": "2019-11-13T00:00:00.000Z",
+  //   "_id": id,
+  //   "pending": false,
+  //   "currency": "CAD"
+  // }
+  // req.user.transactions.push(object)
+  // let result = await req.user.save()
+  // let id = req.params.id;
+  // console.log(req.user)
+  // const result = req.user.transactions.id(id)
+  // console.log(result)
+  return res.status(200).json({ message: 'This works.', result: result });
+}
