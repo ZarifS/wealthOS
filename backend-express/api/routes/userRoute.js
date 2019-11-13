cc5f56a40c3e1ee8eb87ee8b2ce058cbf5322357
ZarifS <zarif.shahriar@gmail.com>
2019-11-12 22:26:43 -0500

    Added testing route

--- backend-express/api/routes/userRoute.js
+++ backend-express/api/routes/userRoute.js
@@ -3,7 +3,8 @@ import { createPublicToken } from '../controllers/plaidController';
 import {
   linkPlaidToUser,
   updateAccounts,
-  setTransactionsForUser
+  setTransactionsForUser,
+  testAPI
 } from '../controllers/userController';
 
 const router = express.Router();
@@ -30,5 +31,8 @@ router.post('/setAccounts', updateAccounts);
 
 // Set user transactions for a the last year - {startDate: "2019-08-01", endDate: "2019-09-01"}
 router.post('/setTransactions', setTransactionsForUser);
+
+// Get user transaction
+router.get('/testAPI/:id', testAPI);
 
 export default router;
