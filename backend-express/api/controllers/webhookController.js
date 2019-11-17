2516f920c1215bab82365229500b8730749ed247
ZarifS <zarif.shahriar@gmail.com>
2019-11-17 18:05:58 -0500

    Moved ngrok listener to addWebhook function

--- backend-express/api/controllers/webhookController.js
+++ backend-express/api/controllers/webhookController.js
@@ -1,14 +1,32 @@
+import ngrok from 'ngrok';
 import { addWebhook, fireTransactionWebhook } from './plaidController';
 
 export const addWebhookToUser = (req, res) => {
-  const { institutionName, webhookURL } = req.body;
+  const { institutionName } = req.body;
   const { accessToken } = req.user.links.get(institutionName);
-  addWebhook(accessToken, webhookURL).then(() => {
-    res.status(200).json({
-      message: 'Updated Webhook Successfully'
+  // Development Server, close all Tunnels, connect ->  Tunnel Connection for Webhook Testing
+  ngrok
+    .kill()
+    .then(() => {
+      console.log('Disconnected from all tunnels. Reconnecting now..');
+      return ngrok.connect(5000);
+    })
+    .then(url => {
+      const webhookURL = `${url}/webhook/`;
+      console.log('Connected to Tunnel at:', webhookURL);
+      return addWebhook(accessToken, webhookURL);
+    })
+    .then(() => {
+      res.status(200).json({
+        message: 'Updated Webhook Successfully'
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json({ message: err.message });
     });
-  });
 };
+
 export const fireWebhook = async (req, res) => {
   const { institutionName } = req.body;
   const { accessToken } = req.user.links.get(institutionName);
