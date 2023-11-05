267816668aa92bc886b3c90456bcac33424dd863
Zarif Shahriar <zarif.shahriar@gmail.com>
2023-11-04 20:21:35 -0400

    Add shadcn and tailwind

--- client-nextjs/src/pages/_app.tsx
+++ client-nextjs/src/pages/_app.tsx
@@ -2,11 +2,23 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import store from '../store';
+import { Inter as FontSans } from "next/font/google"
+import { cn } from '../../@shadcn/lib/utils';
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <div className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}>
+        <Component {...pageProps} />
+      </div>
     </Provider>
   );
 }
